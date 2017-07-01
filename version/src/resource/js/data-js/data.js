d87fb14d5f9b9fa18cd93c717fb4bb1b37287baf
glm196901 <glm196901@gmail.com>
2017-07-01 23:13:35 +0800

    modify var into let in data.js

--- version/src/resource/js/data-js/data.js
+++ version/src/resource/js/data-js/data.js
@@ -602,10 +602,10 @@ const geoCoordMap = {
     '韶关': [113.7964,24.7028]
 };
 $(function(){
-    var migration =  echarts.init(document.getElementById('migration'));
+    const migration =  echarts.init(document.getElementById('migration'));
 
 
-    var BJData = [
+    const BJData = [
         [{name:'北京'}, {name:'大连',value:80}],
         [{name:'北京'}, {name:'南宁',value:70}],
         [{name:'北京'}, {name:'南昌',value:60}],
@@ -616,7 +616,7 @@ $(function(){
         [{name:'北京'}, {name:'常州',value:10}],
     ];
 
-    var SHData = [
+    const SHData = [
         [{name:'上海'},{name:'包头',value:95}],
         [{name:'上海'},{name:'昆明',value:90}],
         [{name:'上海'},{name:'广州',value:80}],
@@ -629,7 +629,7 @@ $(function(){
         [{name:'上海'},{name:'大连',value:10}]
     ];
 
-    var GZData = [
+    const GZData = [
         [{name:'广州'},{name:'福州',value:95}],
         [{name:'广州'},{name:'太原',value:90}],
         [{name:'广州'},{name:'长春',value:80}],
@@ -726,13 +726,13 @@ $(function(){
 
 
 
-    var planePath = 'path://M1705.06,1318.313v-89.254l-319.9-221.799l0.073-208.063c0.521-84.662-26.629-121.796-63.961-121.491c-37.332-0.305-64.482,36.829-63.961,121.491l0.073,208.063l-319.9,221.799v89.254l330.343-157.288l12.238,241.308l-134.449,92.931l0.531,42.034l175.125-42.917l175.125,42.917l0.531-42.034l-134.449-92.931l12.238-241.308L1705.06,1318.313z';
-    var convertData = function (data) {
-        var res = [];
-        for (var i = 0; i < data.length; i++) {
-            var dataItem = data[i];
-            var fromCoord = geoCoordMap[dataItem[0].name];
-            var toCoord = geoCoordMap[dataItem[1].name];
+    const planePath = 'path://M1705.06,1318.313v-89.254l-319.9-221.799l0.073-208.063c0.521-84.662-26.629-121.796-63.961-121.491c-37.332-0.305-64.482,36.829-63.961,121.491l0.073,208.063l-319.9,221.799v89.254l330.343-157.288l12.238,241.308l-134.449,92.931l0.531,42.034l175.125-42.917l175.125,42.917l0.531-42.034l-134.449-92.931l12.238-241.308L1705.06,1318.313z';
+    let convertData = function (data) {
+        let res = [];
+        for (let i = 0; i < data.length; i++) {
+            let dataItem = data[i];
+            let fromCoord = geoCoordMap[dataItem[0].name];
+            let toCoord = geoCoordMap[dataItem[1].name];
             if (fromCoord && toCoord) {
                 res.push({
                     fromName: dataItem[0].name,
